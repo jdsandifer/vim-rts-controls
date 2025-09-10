9814217f721c514803b4645bdac96fad1c9250b1
J.D. Sandifer <sandifer.jd@gmail.com>
2025-09-10 12:51:16 -0700

    Add tailwind, update prettier config

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-      <div>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-      </div>
-      <h1>Vim-like RTS Controls Demo</h1>
-      <p>Demonstrating controls for a real-time strategy game with keyboard commands based on Vim motions.</p>
-      <div className="card">
-        <button onClick={() => {}}>
-          Start the demo
-        </button>
-      </div>
-      Copyright 2025 J.D. Sandifer. See LICENSE for more info.
-    </>
-  )
+	return (
+		<>
+			<div className="flex justify-center">
+				<img src={viteLogo} className="logo" alt="Vite logo" />
+			</div>
+			<h1>Vim-like RTS Controls Demo</h1>
+			<p>
+				Demonstrating controls for a real-time strategy game with keyboard
+				commands based on Vim motions.
+			</p>
+			<div className="card">
+				<button onClick={() => {}}>Start the demo</button>
+			</div>
+			Copyright 2025 J.D. Sandifer. See LICENSE for more info.
+		</>
+	)
 }
 
 export default App
